d6527e1761e6c828c5bc8531b2880493962262a7
umer hanief <78786637+Umerhanief@users.noreply.github.com>
2023-12-20 15:06:14 +0530

    Add files via upload
    this is the gym management system developed using c# asp.net with ms sql server

---
+++ Functions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace GymManagementSystem.Models
+{
+	public class Functions
+
+	{
+		private SqlConnections Con;
+		private SqlCommand cmd;
+		private string ConStr;
+		private DataTable dt;
+		private SqlDataAdapter sda;
+
+		public Funtion()
+		{
+			ConStr = @"Data Source=DESKTOP-KTSVTD4;Initial Catalog=OnlineGymDB.mdf;Integrated Security=True";
+			Con = new SqlConnection(ConStr);
+			cmd = new SqlCommand();
+			cmd.Connection = Con;
+
+
+        }
+		public int setData(string Query)
+		{
+			int cnt = 0;
+			if(Con.State == ConnectionState.Closed)
+			{
+				Con.Open();
+			}
+			cmd.Command = Query;
+			cnt = cmd.ExecuteNonQuery();
+			Con.Close();
+			return cnt;
+		}
+		public DataTable GetData(string Query)
+		{
+			dt = new DataTable();
+			sda = new SqlDataAdapter(Query, ConStr);
+			sda.FILL(dt);
+			return dt;
+		}
+	}
+}
